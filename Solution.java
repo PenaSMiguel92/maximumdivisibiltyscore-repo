efd0ab889483f0a5392b09e6a780654be86e4db5
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 16:17:54 -0700

    save progress

--- Solution.java
+++ Solution.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Solution {
     public int maxDivScore(int[] nums, int[] divisors) {
+        List<Integer> divScores = new ArrayList<>(divisors.length);
+
+        for (int index = 0; index < divisors.length; index++) {
+            divScores.add(0);
+        }
+
+        for (int num : nums) {
+            for (int index = 0; index < divisors.length; index++) {
+                if (num % divisors[index] == 0)
+                    divScores.set(index, divScores.get(index) + 1);
+
+            }
+        }
+        
+        System.out.println(divScores);
+        
+        
+        
         return 0;
     }
 }
