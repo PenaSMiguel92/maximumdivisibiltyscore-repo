b7a4349bc75a7dcce9370901792f1ca2817c8d34
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 17:36:23 -0700

    optimized code
    optimization and greatly reduced runtime from 409 ms to 179 ms, better than 84% of users; similarly, 43.32 mb of memory, better than 98.33% of users.

--- Solution.java
+++ Solution.java
@@ -2,42 +2,20 @@ import java.util.*;
 
 public class Solution {
     public int maxDivScore(int[] nums, int[] divisors) {
-        List<Integer> divScores = new ArrayList<>(divisors.length);
-        List<Integer> maxDivScoreIndeces = new ArrayList<>();
         int maxDivScore = Integer.MIN_VALUE;
         int minDivisor = Integer.MAX_VALUE;
-
-        for (int num : nums) {
-            for (int index = 0; index < divisors.length; index++) {
-
-                if (divScores.size() <= index)
-                    divScores.add(0);
-
+        Arrays.sort(divisors);
+        for (int index = 0; index < divisors.length; index++) {
+            int count = 0;
+            for (int num : nums) {
                 if (num % divisors[index] == 0)
-                    divScores.set(index, divScores.get(index) + 1);
-
+                    count++;
             }
-        }
-
-        for (int i = 0; i < divScores.size(); i++) {
-            if (divScores.get(i) == maxDivScore) {
-                maxDivScoreIndeces.add(i);
-                continue;
-            }
-
-            if (divScores.get(i) > maxDivScore) {
-                maxDivScore = divScores.get(i);
-                maxDivScoreIndeces = new ArrayList<>();
-                maxDivScoreIndeces.add(i);
-                System.gc();
-            }
-        }
-        
-        for (int index : maxDivScoreIndeces) {
-            if (divisors[index] < minDivisor)
+            if ((count > maxDivScore) || (count == maxDivScore && divisors[index] < minDivisor )){
+                maxDivScore = count;
                 minDivisor = divisors[index];
+            }
         }
-
         return minDivisor;
     }
 }
